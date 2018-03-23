20f6be287a31271c43f6f1ae6c6edc5f9f921917
yinqiwen <yinqiwen@gmail.com>
2018-03-23 11:51:30 +0800

    update

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+)
 
+func startHTTPServer(listen string) {
+	http.HandleFunc("/webhook", func(w http.ResponseWriter, r *http.Request) {
+		data, err := ioutil.ReadAll(r.Body)
+		if nil != err {
+			log.Printf("Invalid notify:%v", err)
+			return
+		}
+		log.Printf("Read notify %s", string(data))
+	})
+	err := http.ListenAndServe(listen, nil)
+	if nil != err {
+		log.Printf("Failed to start HTTP server with err:%v", err)
+	}
+}
 func main() {
-	fmt.Printf("Hello, world.\n")
+	startHTTPServer(os.Args[1])
 }
